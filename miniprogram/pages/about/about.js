8c49e43299aab7f02e0c9d6907834d9b16b7c1c1
jingxuehui <jingxh1997@outlook.com>
2021-01-22 10:51:14 +0800

    与我联系页完成

--- miniprogram/pages/about/about.js
+++ miniprogram/pages/about/about.js
@@ -6,8 +6,8 @@ Page({
    */
   data: {
     show: false,
-    url: '../../images/zanshangma.jpg'
-    // url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fdp.gtimg.cn%2Fdiscuzpic%2F0%2Fdiscuz_x5_gamebbs_qq_com_forum_201306_19_1256219xc797y90heepdbh.jpg%2F0&refer=http%3A%2F%2Fdp.gtimg.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1613813628&t=f03f550130b05dd6cc11a12d7cded5e3'
+    ds_url: '../../images/zanshangma.jpg',
+    lx_url: '',
   },
   // 显示弹窗
   showPopup() {
@@ -19,14 +19,26 @@ Page({
     this.setData({ show: false });
   },
 
-  previewImage(e){
-    var cur=e.target.dataset.src;//获取本地一张图片链接
-    console.log(cur);
-		wx.previewImage({
-			current: cur, // 字符串，默认显示urls的第一张
-  		urls: [cur] // 数组，需要预览的图片链接列表
-		})
+  // 打赏码预览
+  onPreviewDS() {
+    wx.previewImage({
+      urls: [this.data.ds_url],
+    })
   },
+  // 二维码预览
+  onPreviewLX() {
+    wx.previewImage({
+      urls: [this.data.lx_url],
+    })
+  },
+  // previewImage(e){
+  //   var cur=e.target.dataset.src;//获取本地一张图片链接
+  //   console.log(cur);
+	// 	wx.previewImage({
+	// 		current: cur, // 字符串，默认显示urls的第一张
+  // 		urls: [cur] // 数组，需要预览的图片链接列表
+	// 	})
+  // },
   
   /**
    * 生命周期函数--监听页面加载
